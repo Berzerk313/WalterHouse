ecc96f77017158fa75616cffdf180b7bab418043
VUE Andrey Chaika <you@example.com>
2024-04-18 18:02:25 +0300

    vite config

--- WalterHouse/vite.config.js
+++ WalterHouse/vite.config.js
@@ -1,10 +1,29 @@
-import { defineConfig } from "vite";
-import FullReload from "vite-plugin-full-reload";
-import injectHTML from "vite-plugin-html-inject";
-import handlebars from "vite-plugin-handlebars";
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
+import FullReload from 'vite-plugin-full-reload';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
-  plugins: [FullReload(["config/routes.rb", "app/views/**/*"])],
-  plugins: [injectHTML()],
-  plugins: [handlebars()],
+    build: {
+        target: 'es2022',
+        outDir: 'build',
+    },
+    base: 'WalterHouse',
+
+    server: {
+        port: 3025,
+        host: '0.0.0.0',
+        hmr: true,
+    },
+    plugins: [
+        injectHTML(),
+        handlebars({
+            reloadOnPartialChange: true,
+            partialDirectory: resolve(__dirname, 'src/html'),
+        }),
+
+        FullReload('src/html/**/*', { delay: 0 }),
+
+    ],
 });
