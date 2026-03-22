73aabc0891e3d116fb2e58dee898112a03853df6
Berzerk313 <konor234313@gmail.com>
2026-03-22 15:43:08 +0200

    settings and structure project

---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+import '@/scss/main.scss';
+import '@/js/modules/header.js';
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   server: {
-    open: true
-  }
-}) 
+    open: true,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
