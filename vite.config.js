e60fceb0775cece732a5ff40bd4a3bc98722341b
Berzerk313 <konor234313@gmail.com>
2026-04-05 17:43:21 +0300

    try repair config for dev and buld version

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
@@ -8,63 +8,83 @@ import injectHTML from 'vite-plugin-html-inject';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import viteImagemin from 'vite-plugin-imagemin';
 
-const baseUrl = process.env.VITE_BASE_URL || '/';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  base: baseUrl,
-  build: {
-    target: 'es2022',
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@assets': resolve(__dirname, 'src/assets'),
-      '@partials': resolve(__dirname, 'src/html/html-partials'),
+  const baseUrl = env.VITE_BASE_URL || '/';
+  const isBuild = mode === 'production';
+
+  console.log('BASE URL:', baseUrl, 'IS BUILD:', isBuild);
+
+  return {
+    base: baseUrl,
+    root: '.',
+    publicDir: 'src/assets',
+    build: {
+      target: 'es2022',
+      outDir: 'dist',
     },
-  },
-
-  server: {
-    port: 3025,
-    host: '0.0.0.0',
-    hmr: true,
-    allowedHosts: [''],
-  },
-
-  plugins: [
-    injectHTML(),
-    handlebars({
-      reloadOnPartialChange: true,
-      partialDirectory: resolve(__dirname, 'src/html/html-partials'),
-      context: {
-        baseUrl: 'baseUrl',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '@assets': resolve(__dirname, 'src/assets'),
+        '@partials': resolve(__dirname, 'src/html/html-partials'),
       },
-    }),
+    },
 
-    FullReload('src/html/html-partials/**/*', { delay: 0 }),
+    server: {
+      port: 3025,
+      host: '0.0.0.0',
+      hmr: true,
+      allowedHosts: [''],
+    },
 
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/**/*',
-          dest: 'assets',
-        },
-      ],
-    }),
+    plugins: [
+      injectHTML(),
+      handlebars({
+        entry: resolve(__dirname, 'index.html'),
+        partialDirectory: resolve(__dirname, 'src/html/html-partials'),
+        extensions: ['.handlebars', '.hbs', '.html'], // путь к partials
+        reloadOnPartialChange: true, // для dev, можно оставить
+        context: () => ({
+          baseUrl,
+          isBuild: mode === 'production',
+        }),
+      }),
+      // handlebars({
+      //   reloadOnPartialChange: true,
+      //   partialDirectory: resolve(__dirname, 'src/html/html-partials'),
+      //   context: {
+      //     baseUrl,
+      //     isBuild,
+      //   },
+      // }),
 
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
-      symbolId: 'icon-[name]', // будет id="icon-phone"
-    }),
+      FullReload('src/html/html-partials/**/*', { delay: 0 }),
 
-    viteImagemin({
-      webp: {
-        quality: 75, // настрой качество WebP
-      },
-      gifsicle: false,
-      optipng: false,
-      mozjpeg: false,
-      svgo: false,
-    }),
-  ],
+      // viteStaticCopy({
+      //   targets: [
+      //     {
+      //       src: 'src/assets/images/**/*',
+      //       dest: 'images',
+      //     },
+      //   ],
+      // }),
+
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
+        symbolId: 'icon-[name]', // будет id="icon-phone"
+      }),
+     isBuild &&
+        viteImagemin({
+          webp: {
+            quality: 75,
+          },
+          gifsicle: false,
+          optipng: false,
+          mozjpeg: false,
+          svgo: false,
+        }),
+    ].filter(Boolean), // фильтруем false, когда dev
+  };
 });
